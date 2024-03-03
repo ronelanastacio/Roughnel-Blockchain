2e28f5440dee21809dc00842f15fba74271d2288
ronelanastacio <89885308+ronelanastacio@users.noreply.github.com>
2024-03-03 14:08:13 +0000

    update

--- Roughnel-project/contracts/Roughnel.sol
+++ Roughnel-project/contracts/Roughnel.sol
@@ -5,68 +5,66 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Roughnel is ERC20, Ownable {
+    using SafeMath for uint256;
 
     mapping(address => uint256) private _stakes;
     mapping(address => uint256) private _lastStakeTimestamp;
     uint256 private _rewardRate = 1;
-    uint256 private lockInPeriod = 60; //1 min
+    uint256 private lockInPeriod = 60; // 1 min
 
-    constructor(address initialOwner) 
-        ERC20("Roughnel", "RAJ") 
+    constructor(address initialOwner)
+        ERC20("Roughnel", "RAJ")
         Ownable(initialOwner)
     {}
 
-    function mint(address to, uint256 amount) public {
-        uint256 adjustedAmount = amount * 1e18;
+    function mint(address to, uint256 amount) external onlyOwner {
+        uint256 adjustedAmount = amount.mul(1e18);
         _mint(to, adjustedAmount);
     }
 
-    function stake(uint256 amount) public {
-        uint256 adjustedAmount = amount * 1e18;
+    function stake(uint256 amount) external {
+        uint256 adjustedAmount = amount.mul(1e18);
 
         require(adjustedAmount > 0, "Cannot stake 0 tokens");
         require(balanceOf(msg.sender) >= adjustedAmount, "Insufficient balance");
 
-        _stakes[msg.sender] += adjustedAmount;
+        _stakes[msg.sender] = _stakes[msg.sender].add(adjustedAmount);
         _lastStakeTimestamp[msg.sender] = block.timestamp;
         _transfer(msg.sender, address(this), adjustedAmount);
-  }
+    }
 
-    function getStake(address account) public view returns (uint256) {
+    function getStake(address account) external view returns (uint256) {
         uint256 stakedInWei = _stakes[account];
-        uint256 stakedInEth = stakedInWei / 1e18;
-        return stakedInEth;
-  }
+        return stakedInWei.div(1e18);
+    }
 
-    function withdraw() public {
-        require(block.timestamp > (_lastStakeTimestamp[msg.sender] + lockInPeriod), "You cannot withdraw funds, you are still in the lock in period");
+    function withdraw() external {
+        require(
+            block.timestamp > (_lastStakeTimestamp[msg.sender] + lockInPeriod),
+            "Cannot withdraw, still in the lock-in period"
+        );
         require(_stakes[msg.sender] > 0, "No staked tokens");
 
         uint256 stakedAmount = _stakes[msg.sender];
-        uint256 reward = ((block.timestamp - _lastStakeTimestamp[msg.sender]) * _rewardRate) * 1e18;
+        uint256 reward = (block.timestamp.sub(_lastStakeTimestamp[msg.sender])).mul(_rewardRate).mul(1e18);
 
         _stakes[msg.sender] = 0;
         _transfer(address(this), msg.sender, stakedAmount);
         _mint(msg.sender, reward);
-  }
+    }
 
-    function getWithdraw(address account) public view returns (uint256) {
-        uint256 stakedAmount = _stakes[msg.sender] / 1e18;
-        uint256 reward = ((block.timestamp - _lastStakeTimestamp[account]) * _rewardRate);
+    function getWithdraw(address account) external view returns (uint256) {
+        uint256 stakedAmount = _stakes[account].div(1e18);
+        uint256 reward = (block.timestamp.sub(_lastStakeTimestamp[account])).mul(_rewardRate);
 
-        uint256 total = reward + stakedAmount; 
-        return total;
-  }
+        return stakedAmount.add(reward);
+    }
 
-     function getElapsedStakeTime(address account) public view returns (uint256) {
-        uint256 time = (block.timestamp - _lastStakeTimestamp[account]);
-        return time;
-  } 
+    function getElapsedStakeTime(address account) external view returns (uint256) {
+        return block.timestamp.sub(_lastStakeTimestamp[account]);
+    }
 
-    function getLastStakeTimestamp(address account) public view returns (uint256) {
+    function getLastStakeTimestamp(address account) external view returns (uint256) {
         return _lastStakeTimestamp[account];
-  }
-
-
-    
+    }
 }
